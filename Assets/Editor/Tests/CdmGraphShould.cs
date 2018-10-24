c2151e05317343a2493cb4d5d0cf6c501112a7bb
KevinSmall <kevin_small@hotmail.com>
2018-10-24 21:49:34 +0100

    Added Unity Test Runner unit test project skeleton

---
+++ Assets/Editor/Tests/CdmGraphShould.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using UnityEngine.TestTools;
+
+public class CdmGraphShould
+{
+
+   [Test]
+   public void InitialiseCorrectly()
+   {
+      // Use the Assert class to test conditions.
+   }
+
+   [Test]
+   public void NewTestScriptSimplePasses()
+   {
+      // Use the Assert class to test conditions.
+   }
+
+   // A UnityTest behaves like a coroutine in PlayMode
+   // and allows you to yield null to skip a frame in EditMode
+   [UnityTest]
+   public IEnumerator NewTestScriptWithEnumeratorPasses()
+   {
+      // Use the Assert class to test conditions.
+      // yield to skip a frame
+      yield return null;
+   }
+}
